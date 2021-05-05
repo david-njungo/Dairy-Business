4ff640b6266630135c3babef006e1d4f2edd5585
davidnjungo-lab <david.kimani@student.moringaschool.com>
2021-05-05 05:10:22 +0300

    add the calls for the incomeovertime function

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ production("D",572)
                 total=(510+308+486+572);
                 console.log("The total production is" +" "+ total+" "+"litres per day");
             } 
-            var incomeOvertime=function(sellingPrice,time){
-                console.log("Your"+" "+time+" "+"income will be"+" "+sellingPrice);
-            }    
+                var incomeOvertime=function(sellingPrice,time){
+                    console.log("Your"+" "+time+" "+"income will be"+" "+sellingPrice);
+                } 
+                incomeOvertime([1876*45*7],"weekly")
+                incomeOvertime([1876*45*366],"yearly")   
