3e142684d029e23dfafe0468114fb199a819f113
davidnjungo-lab <david.kimani@student.moringaschool.com>
2021-05-05 04:56:49 +0300

    add the calls of the production function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var production=function(shedName,value){
     console.log("Your production in shed"+" "+shedName+ " "+value+" " + "litres per day");
    }
+production("A",510)
+production("B",308)
+production("C",486)
+production("D",572)
